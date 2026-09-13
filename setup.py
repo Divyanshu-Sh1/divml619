ef86d8d2bd8b64772c38a09b2d91e3854431bb87
Divyanshu-Sh1 <divyanshudcis@gmail.com>
2026-09-13 12:37:10 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name="mlproject2",
+version="0.0.1",
+authuor_name="divyasnhu"
+authour_email="divyanshudcis@gmail.com"
+packages=find_packages(),
+install_requires=["numpy","pandas","seaborn"]
+)
