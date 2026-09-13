53f7497afbf62ad80b86c3e8dce32adc025ba350
Divyanshu-Sh1 <divyanshudcis@gmail.com>
2026-09-13 12:40:58 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import find_packages,setup
+hypen_e_dot="-e ."
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function returns list of reqirements
 
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines("requirements.txt")
+        requirements=[req.replace("/n"," ") for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+    return requirements
 setup(
 name="mlproject2",
 version="0.0.1",
-authuor_name="divyasnhu"
-authour_email="divyanshudcis@gmail.com"
+authuor_name="divyasnhu",
+authour_email="divyanshudcis@gmail.com",
 packages=find_packages(),
-install_requires=["numpy","pandas","seaborn"]
+install_requires=get_requirements('requirements.txt')
 )
